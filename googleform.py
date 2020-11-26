ccd1de01076770a56dbde7a466fd8eb6aebbf908
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-11-26 22:46:22 +0530

    no resp update

--- googleform.py
+++ googleform.py
@@ -20,22 +20,26 @@ def get_fields(link):
     soup = get_soup(link)
     data = soup.findAll("script", {"nonce": True})[3].string
     data = json.loads(data[27:-1])[1]
-    title = data[-11]
+    # pprint(data)
+    title = data[-9]
     desc = data[0]
     items = []
     for item in data[1]:
-        _id = "entry."+str(item[4][0][0])
-        question = item[1]
-        _type = item[3]
-        if item[4][0][2] == 1:
-            req="required"
-        else:
-            req=""
-        options = []
-        if _type in [2, 3, 4]:
-            for option in item[4][0][1]:
-                options.append({"option": option[0], "type": option[4]})
-        items.append({"id": _id, "question": question, "type": _type, "req": req, "options": options})
+        try:
+            _id = "entry."+str(item[4][0][0])
+            question = item[1]
+            _type = item[3]
+            if item[4][0][2] == 1:
+                req="required"
+            else:
+                req=""
+            options = []
+            if _type in [2, 3, 4]:
+                for option in item[4][0][1]:
+                    options.append({"option": option[0], "type": option[4]})
+            items.append({"id": _id, "question": question, "type": _type, "req": req, "options": options})
+        except:
+            pass
     return {"title": title, "desc": desc, "items": items, "link": link}
 
 def faker_input(_type):
@@ -82,5 +86,6 @@ def spam_form(response):
             else:
                 payload[key] = value
         requests.post(link, data=payload)
+        print(payload)
         sleep(0.3)
     return
